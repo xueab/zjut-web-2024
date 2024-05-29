37764c4d3aa9530c7e4144285baac2c5b3428749
yq <2416170020@qq.com>
2024-05-29 21:21:30 +0800

    complete peoplemange and salary

--- webwork/src/main/java/controller/LoginServlet.java
+++ webwork/src/main/java/controller/LoginServlet.java
@@ -1,5 +1,7 @@
 package controller;
 
+import service.LoginService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +13,43 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        LoginService s = new LoginService();
+        // 检查用户名和密码是否正确
+        if (s.checkUser(username, password)) {
+            // 确定用户角色
+            String role = s.getUserRole(username, password);
+            if (role.equals("人事管理员")) {
+                // 重定向到人事管理员界面
+                resp.sendRedirect("peoplemange.jsp");
+            }
+            else if (role.equals("财务管理员")) {
+                // 重定向到财务管理员界面
+                resp.sendRedirect("salaryMange.jsp");
+            }
+            else if (role.equals("总经理")) {
+                // 重定向到总经理界面
+                resp.sendRedirect("leader.jsp");
+            }
+            else if (role.equals("系统管理员")) {
+                // 重定向到系统管理员界面
+                resp.sendRedirect("management.jsp");
+            }
+            else if (role.equals("审计员")) {
+                //
+                //resp.sendRedirect("peopleMange.jsp");
+            }
+        }
+        else {
+
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        doGet(req, resp);
     }
 }
