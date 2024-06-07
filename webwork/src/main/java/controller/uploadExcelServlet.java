3c216d8dca102f9855c212605bf9e1f2c4ef72a7
yq <2416170020@qq.com>
2024-06-07 20:36:55 +0800

    complete excel upload

--- webwork/src/main/java/controller/uploadExcelServlet.java
+++ webwork/src/main/java/controller/uploadExcelServlet.java
@@ -6,6 +6,7 @@ import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import service.SalaryService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,25 +20,23 @@ import java.io.InputStream;
 
 // 上传excel
 @WebServlet("/uploadExcelServlet")
+@MultipartConfig    // 处理文件上传
 public class uploadExcelServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SalaryService s = new SalaryService();
-
+        // 从HTTP请求中获取名为 "file" 的 Part 对象。 Part 对象代表上传的文件或表单字段。
+        // req.getPart("file") 是从 HttpServletRequest 对象 req 中获取 file 字段对应的 Part
+        // file 是前端表单中文件上传控件的名称
         Part filePart = req.getPart("file");
-        String fileName = getFileName(filePart);
-        // 保存路径
-        // getServletContext().getRealPath("/")获取了应用程序的根目录，在其后追加了 "uploads" 文件夹和文件名
-        // 使用了 File.separator 来保证路径在不同操作系统上的兼容性。
-        String savePath = getServletContext().getRealPath("/") + "uploads" + File.separator + fileName;
-
 
         // 从上传文件的Part中获取输入流
         InputStream file = filePart.getInputStream();
 
         s.uploadExcel(file);
 
-        resp.getWriter().println("File uploaded to: " + savePath);
+        resp.sendRedirect(req.getContextPath() + "/financialManager.jsp");
+
     }
 
     @Override
