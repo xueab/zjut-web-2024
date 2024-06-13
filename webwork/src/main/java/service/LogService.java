556ab71a30f7b02a352140bddc294fccf4495522
yq <2416170020@qq.com>
2024-06-13 11:18:48 +0800

    test

--- webwork/src/main/java/service/LogService.java
+++ webwork/src/main/java/service/LogService.java
@@ -2,7 +2,10 @@ package service;
 
 import dao.LogDao;
 import model.Log;
+import model.Salary;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class LogService {
@@ -21,5 +24,28 @@ public class LogService {
 
     public List<Log> selectByPage(int page) {
         return logDao.selectByPage(page * 10);
+    }
+
+    public List<Log> searchByName(String name) {
+        return logDao.serchByName(name);
+    }
+
+    public List<Salary> searchByDate(Date beginTime, Date endTime) {
+        // 将Date转换成Calendar
+        // 创建一个 Calendar 对象并设置时间为当前 date
+        Calendar calendarBegin = Calendar.getInstance();
+        calendarBegin.setTime(beginTime);
+        Calendar calendarEnd = Calendar.getInstance();
+        calendarEnd.setTime(endTime);
+
+        // 获取年份
+        int beginYear = calendarBegin.get(Calendar.YEAR);
+        int endYear = calendarEnd.get(Calendar.YEAR);
+
+        // 获取月份
+        int beginMonth = calendarBegin.get(Calendar.MONTH) + 1;
+        int endMonth = calendarEnd.get(Calendar.MONTH) + 1;
+
+        logDao.searchByDate(beginYear, beginMonth, endYear, endMonth);
     }
 }
